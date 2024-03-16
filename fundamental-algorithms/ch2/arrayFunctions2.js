5d3b1667485c7b170a276e64a6de989ab8e224a0
Benji Hix <benji.hix@outlook.com>
2024-03-16 15:03:27 -0700

    algos

--- fundamental-algorithms/ch2/arrayFunctions2.js
+++ fundamental-algorithms/ch2/arrayFunctions2.js
@@ -1,7 +1,6 @@
 //! Do not use built-in array methods 
 
-//~ Reverse array
-
+//* ------------------------------ reverse array ----------------------------- */
 function reverseArr(arr) {
     const lastIndex = arr.length - 1
 
@@ -17,22 +16,41 @@ function reverseArr(arr) {
 
 console.log(reverseArr([1, 2, 3, 4, 5]));
 
-//~ Remove negatives: return a given array with all negative
-//~ values removed 
+//* ---------------------------- remove negatives ---------------------------- */
+//` Return a given array with all negative
+//` values removed 
 
 function removeNegatives(arr) {
-    while (arr.length && arr[arr.length - 1] < 0) {
-        arr.pop();
-    }
-
-    for (let i = arr.length - 1; i >= 0; i++) {
-        if (arr[i] < 0) {
-            arr[i] = arr[arr.length - 1];
-            arr.pop();
+    let j = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            arr[j] = arr[i]
+            j++
         }
     }
-
+    while (j < arr.length) {
+        arr.pop();
+    }
     return arr;
 }
 
-console.log(removeNegatives(1, -2, 3, -4, 5, 6, -7, -8));
+console.log(removeNegatives([1, -2, 3, -4, 5, 6, -7, -8]));
+
+//* ----------------------------- skyline heights ---------------------------- */
+//` given an array of values that represent building heights,
+//` return an array of visible buildings
+
+function visibleBuildings(arr) {
+    let max = arr[0];
+    const visibleValues = [arr[0]];
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+            visibleValues.push(arr[i])
+        }
+    }
+    return visibleValues
+}
+
+console.log(visibleBuildings([1, -1, 5, 2, 7, 3]));
